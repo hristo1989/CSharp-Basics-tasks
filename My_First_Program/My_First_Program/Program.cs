e4d4d9518197cc419017d7bb3925e127a45bbad3
hristo1989 <hristohlihov@gmail.com>
2015-04-28 23:12:07 +0300

    My_First_Program

---
+++ My_First_Program/My_First_Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_First_Program
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Hello World! This is my first program.");
+            Console.ReadLine ();
+        }
+    }
+}
